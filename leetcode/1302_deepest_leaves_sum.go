a952942ece096b303c999583f2c4a31f921e2026
qiandahao <qiandahao@sensetime.com>
2022-02-18 18:09:14 +0800

    404

--- leetcode/1302_deepest_leaves_sum.go
+++ leetcode/1302_deepest_leaves_sum.go
@@ -8,56 +8,24 @@
  */
 
  func deepestLeavesSum(root *TreeNode) int {
-	queueP := make([]*TreeNode, 0)
-	queueQ := make([]*TreeNode, 0)
-	queueP = append(queueP, root)
-	clearP := true
-	sum, res := 0, 0
-	for len(queueP) != 0 || len(queueQ) != 0 {
-		if clearP {
-			if len(queueP) == 0 {
-				clearP = false
-				if sum != 0 {
-					res = sum
-				}
-				sum = 0
-			} else {
-				temp := queueP[0]
-				sum += temp.Val
-				n := len(queueP)
-				queueP = queueP[1:n]
-				if temp.Left != nil {
-                    queueQ = append(queueQ, temp.Left)
-				}
-				if temp.Right != nil {
-                    queueQ = append(queueQ, temp.Right)
-				}
-			}
-		} else {
-			if len(queueQ) == 0 {
-				clearP = true
-				if sum != 0 {
-					res = sum
-				}
-				sum = 0
-			} else {
-				temp := queueQ[0]
-				n := len(queueQ)
-				sum += temp.Val
-				queueQ = queueQ[1:n]
-				if temp.Left != nil {
-					queueP = append(queueP, temp.Left)
-				}
-				if temp.Right != nil {
-					queueP = append(queueP, temp.Right)
-				}
-			}
-		}
-		
-	}
-     if sum != 0 {
-         return sum
-     } else {
-         return res
-     }
+    queue := make([]*TreeNode, 0)
+    queue = append(queue, root)
+    sum := 0
+    for len(queue) != 0 {
+        sum = 0
+        index := len(queue)
+        for i:=0; i < index; i++ {
+            temp := queue[i]
+            sum += temp.Val
+            if temp.Left != nil {
+                queue = append(queue, temp.Left)
+            }
+            if temp.Right != nil{
+                queue = append(queue, temp.Right)
+            }
+        }
+        n := len(queue)
+        queue = queue[index:n]
+    }
+    return sum
 }
